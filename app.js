8b0829ecb8a1801bdd1edd0072212ca4b1b50eaa
Aakash Mishra <aakashmishra.281@gmail.com>
2023-05-30 19:11:29 +0530

    Response headers to be attatched to avoid CORS

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  //second argument allows to control which domains should actually have access. where browser should allow this.  
+
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+  //second argument tells which headers incoming requests may have so that they are handled.
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  //controls which http methods may be attatched to http requests by the frontEnd.
+
+  next();
+});
+
 const placesRoutes = require('./routes/places-routes');
 const userRoutes = require('./routes/users-routes');
 const HttpError = require("./models/http-errors");
@@ -29,10 +42,12 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500)
   res.json({ message: error.message || "An unknown error occured." });
 });
+
+
 //we want to make sure our backend is connected to database before server is connected
 //if not connected then only start server.s
 mongoose
-  .connect('mongodb+srv://sky:hellosky@cluster0.kfazawl.mongodb.net/Blogs?retryWrites=true&w=majority')
+  .connect('mongodb+srv://sonu:hellosonu@cluster0.kfazawl.mongodb.net/MERN?retryWrites=true&w=majority')
   .then(() => {
     app.listen(5000, function () {
       console.log("Server started on port 5000");
